e3ee559a89b53a1993b0a6f4febd2319c814b986
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-03 16:20:05 +0530

    Protecting the route and saving credentials

--- src/pages/Login/Login_Form.js
+++ src/pages/Login/Login_Form.js
@@ -1,4 +1,83 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import CTA from "../../components/Common_Components/CTA";
+// import Toggle_login_signup from "../../components/Common_Components/Toggle_login_signup";
+// import Google_singup_CTA from "../../components/Google_singup_CTA";
+// import QRCodeComponent from "../../components/QRCodeComponent";
+
+// const Login_Form = () => {
+//   const [focusedInput, setFocusedInput] = useState(null);
+//   // const [showQR, setShowQR] = useState(false);
+//   // const [name,setName] = useState(true)
+
+// //  const show = () =>{
+
+// //   setName(!name)
+
+// //  }
+
+//   return (
+//     <div className="w-[290px] mx-auto flex flex-col gap-[18px]">
+//       <section className="w-full">
+//         <form className="flex flex-col gap-[18px]">
+//           <div
+//             className={`w-[290px] h-[56px] ${
+//               focusedInput === "username" ? "border-none" : "border-2 border-black"
+//             } bg-white`}
+//           >
+//             <input
+//               type="text"
+//               placeholder="Username"
+//               required
+//               onFocus={() => setFocusedInput("username")}
+//               onBlur={() => setFocusedInput(null)}
+//               className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
+//                 focusedInput === "username" ? "border-2 border-black" : "border-none"
+//               } bg-white`}
+//               style={{ boxSizing: "border-box" }}
+//             />
+//           </div>
+
+//           <div
+//             className={`w-[290px] h-[56px] ${
+//               focusedInput === "password" ? "border-none" : "border-2 border-black"
+//             } bg-white`}
+//           >
+//             <input
+//               type="password"
+//               placeholder="Password"
+//               required
+//               onFocus={() => setFocusedInput("password")}
+//               onBlur={() => setFocusedInput(null)}
+//               className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
+//                 focusedInput === "password" ? "border-2 border-black" : "border-none"
+//               } bg-white`}
+//               style={{ boxSizing: "border-box" }}
+//             />
+//           </div>
+
+//           <CTA btn_name="Log in" />
+//           {/* <button onClick={show}>{!name ? "scan" : "close"}</button> */}
+//           {/* {
+//                 name ? <QRCodeComponent />: ""
+//               } */}
+//           <Google_singup_CTA btn_name="Login with Google" />
+//         </form>
+//       </section>
+
+//       <Toggle_login_signup
+//         notify="Don’t have an account?"
+//         redirect_To=" Sign up"
+//         redirect_path="/signup"
+//       />
+
+//     </div>
+//   );
+// };
+
+// export default Login_Form;
+
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import CTA from "../../components/Common_Components/CTA";
 import Toggle_login_signup from "../../components/Common_Components/Toggle_login_signup";
 import Google_singup_CTA from "../../components/Google_singup_CTA";
@@ -6,14 +85,14 @@ import QRCodeComponent from "../../components/QRCodeComponent";
 
 const Login_Form = () => {
   const [focusedInput, setFocusedInput] = useState(null);
-  // const [showQR, setShowQR] = useState(false);
-  // const [name,setName] = useState(true)
+  const navigate = useNavigate();
 
-//  const show = () =>{
-
-//   setName(!name)
-
-//  }
+  useEffect(() => {
+    const storedData = localStorage.getItem("userData");
+    if (storedData) {
+      navigate("/");
+    }
+  }, [navigate]);
 
   return (
     <div className="w-[290px] mx-auto flex flex-col gap-[18px]">
@@ -21,7 +100,9 @@ const Login_Form = () => {
         <form className="flex flex-col gap-[18px]">
           <div
             className={`w-[290px] h-[56px] ${
-              focusedInput === "username" ? "border-none" : "border-2 border-black"
+              focusedInput === "username"
+                ? "border-none"
+                : "border-2 border-black"
             } bg-white`}
           >
             <input
@@ -31,7 +112,9 @@ const Login_Form = () => {
               onFocus={() => setFocusedInput("username")}
               onBlur={() => setFocusedInput(null)}
               className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
-                focusedInput === "username" ? "border-2 border-black" : "border-none"
+                focusedInput === "username"
+                  ? "border-2 border-black"
+                  : "border-none"
               } bg-white`}
               style={{ boxSizing: "border-box" }}
             />
@@ -39,7 +122,9 @@ const Login_Form = () => {
 
           <div
             className={`w-[290px] h-[56px] ${
-              focusedInput === "password" ? "border-none" : "border-2 border-black"
+              focusedInput === "password"
+                ? "border-none"
+                : "border-2 border-black"
             } bg-white`}
           >
             <input
@@ -49,17 +134,15 @@ const Login_Form = () => {
               onFocus={() => setFocusedInput("password")}
               onBlur={() => setFocusedInput(null)}
               className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
-                focusedInput === "password" ? "border-2 border-black" : "border-none"
+                focusedInput === "password"
+                  ? "border-2 border-black"
+                  : "border-none"
               } bg-white`}
               style={{ boxSizing: "border-box" }}
             />
           </div>
 
           <CTA btn_name="Log in" />
-          {/* <button onClick={show}>{!name ? "scan" : "close"}</button> */}
-          {/* {
-                name ? <QRCodeComponent />: ""
-              } */}
           <Google_singup_CTA btn_name="Login with Google" />
         </form>
       </section>
@@ -69,9 +152,6 @@ const Login_Form = () => {
         redirect_To=" Sign up"
         redirect_path="/signup"
       />
-
-            
-
     </div>
   );
 };
--- src/pages/Signup/Form.js
+++ src/pages/Signup/Form.js
@@ -1,89 +1,219 @@
-import React,{useState} from 'react'
-import CTA from '../../components/Common_Components/CTA';
-import Toggle_login_signup from '../../components/Common_Components/Toggle_login_signup';
-import Google_singup_CTA from '../../components/Google_singup_CTA';
+// import React,{useState} from 'react'
+// import CTA from '../../components/Common_Components/CTA';
+// import Toggle_login_signup from '../../components/Common_Components/Toggle_login_signup';
+// import Google_singup_CTA from '../../components/Google_singup_CTA';
 
+// const Form = () => {
+//     const [focusedInput, setFocusedInput] = useState(null);
+//     return (
+//       <div className="w-[290px]  mx-auto flex flex-col gap-[18px]  ">
+//         <section className=" w-full">
+//           <form className="flex flex-col gap-[18px] ">
+//             <div
+//               className={`w-[290px] h-[56px] ${
+//                 focusedInput === "email" ? "border-none" : "border-2 border-black"
+//               } bg-white`}
+//             >
+//               <input
+//                 type="email"
+//                 placeholder="Email"
+//                 required
+//                 onFocus={() => setFocusedInput("email")}
+//                 onBlur={() => setFocusedInput(null)}
+//                 className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78]  p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
+//                   focusedInput === "email"
+//                     ? "border-2 border-black"
+//                     : "border-none"
+//                 } bg-white`}
+//                 style={{ boxSizing: "border-box" }}
+//               />
+//             </div>
+
+//             <div
+//               className={`w-[290px] h-[56px] ${
+//                 focusedInput === "username"
+//                   ? "border-none"
+//                   : "border-2 border-black"
+//               } bg-white`}
+//             >
+//               <input
+//                 type="text"
+//                 placeholder="Username"
+//                 required
+//                 onFocus={() => setFocusedInput("username")}
+//                 onBlur={() => setFocusedInput(null)}
+//                 className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
+//                   focusedInput === "username"
+//                     ? "border-2 border-black"
+//                     : "border-none"
+//                 } bg-white`}
+//                 style={{ boxSizing: "border-box" }}
+//               />
+//             </div>
+
+//             <div
+//               className={`w-[290px] h-[56px] ${
+//                 focusedInput === "password"
+//                   ? "border-none"
+//                   : "border-2 border-black"
+//               } bg-white`}
+//             >
+//               <input
+//                 type="password"
+//                 placeholder="Password"
+//                 required
+//                 onFocus={() => setFocusedInput("password")}
+//                 onBlur={() => setFocusedInput(null)}
+//                 className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
+//                   focusedInput === "password"
+//                     ? "border-2 border-black"
+//                     : "border-none"
+//                 } bg-white`}
+//                 we
+//                 are
+//                 style={{ boxSizing: "border-box" }}
+//               />
+//             </div>
+
+//           <CTA btn_name="Create account"/>
+//           <Google_singup_CTA btn_name="Continue with Google"/>
+//           </form>
+//         </section>
+
+//        <Toggle_login_signup notify="Already have an account ?" redirect_To=" Log in" redirect_path="/login"/>
+//       </div>
+//     );
+//   };
+
+//   export default Form;
+
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import CTA from "../../components/Common_Components/CTA";
+import Toggle_login_signup from "../../components/Common_Components/Toggle_login_signup";
+import Google_singup_CTA from "../../components/Google_singup_CTA";
 
 const Form = () => {
-    const [focusedInput, setFocusedInput] = useState(null);
-    return (
-      <div className="w-[290px]  mx-auto flex flex-col gap-[18px]  ">
-        <section className=" w-full">
-          <form className="flex flex-col gap-[18px] ">
-            <div
-              className={`w-[290px] h-[56px] ${
-                focusedInput === "email" ? "border-none" : "border-2 border-black"
-              } bg-white`}
-            >
-              <input
-                type="email"
-                placeholder="Email"
-                required
-                onFocus={() => setFocusedInput("email")}
-                onBlur={() => setFocusedInput(null)}
-                className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78]  p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
-                  focusedInput === "email"
-                    ? "border-2 border-black"
-                    : "border-none"
-                } bg-white`}
-                style={{ boxSizing: "border-box" }}
-              />
-            </div>
-  
-            <div
-              className={`w-[290px] h-[56px] ${
-                focusedInput === "username"
-                  ? "border-none"
-                  : "border-2 border-black"
-              } bg-white`}
-            >
-              <input
-                type="text"
-                placeholder="Username"
-                required
-                onFocus={() => setFocusedInput("username")}
-                onBlur={() => setFocusedInput(null)}
-                className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
-                  focusedInput === "username"
-                    ? "border-2 border-black"
-                    : "border-none"
-                } bg-white`}
-                style={{ boxSizing: "border-box" }}
-              />
-            </div>
-  
-            <div
-              className={`w-[290px] h-[56px] ${
-                focusedInput === "password"
-                  ? "border-none"
-                  : "border-2 border-black"
-              } bg-white`}
-            >
-              <input
-                type="password"
-                placeholder="Password"
-                required
-                onFocus={() => setFocusedInput("password")}
-                onBlur={() => setFocusedInput(null)}
-                className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
-                  focusedInput === "password"
-                    ? "border-2 border-black"
-                    : "border-none"
-                } bg-white`}
-                we
-                are
-                style={{ boxSizing: "border-box" }}
-              />
-            </div>
-  
-          <CTA btn_name="Create account"/>
-          <Google_singup_CTA btn_name="Continue with Google"/>
-          </form>
-        </section>
-  
-       <Toggle_login_signup notify="Already have an account ?" redirect_To=" Log in" redirect_path="/login"/>
-      </div>
-    );
+  const [focusedInput, setFocusedInput] = useState(null);
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("userData");
+    if (storedData) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
-  
-  export default Form;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Save data to local storage
+    localStorage.setItem("userData", JSON.stringify(formData));
+    // Redirect to home page after signup
+    navigate("/");
+  };
+
+  return (
+    <div className="w-[290px] mx-auto flex flex-col gap-[18px]">
+      <section className="w-full">
+        <form className="flex flex-col gap-[18px]" onSubmit={handleSubmit}>
+          <div
+            className={`w-[290px] h-[56px] ${
+              focusedInput === "email" ? "border-none" : "border-2 border-black"
+            } bg-white`}
+          >
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+              onFocus={() => setFocusedInput("email")}
+              onBlur={() => setFocusedInput(null)}
+              onChange={handleChange}
+              value={formData.email}
+              className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
+                focusedInput === "email"
+                  ? "border-2 border-black"
+                  : "border-none"
+              } bg-white`}
+              style={{ boxSizing: "border-box" }}
+            />
+          </div>
+
+          <div
+            className={`w-[290px] h-[56px] ${
+              focusedInput === "username"
+                ? "border-none"
+                : "border-2 border-black"
+            } bg-white`}
+          >
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              required
+              onFocus={() => setFocusedInput("username")}
+              onBlur={() => setFocusedInput(null)}
+              onChange={handleChange}
+              value={formData.username}
+              className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
+                focusedInput === "username"
+                  ? "border-2 border-black"
+                  : "border-none"
+              } bg-white`}
+              style={{ boxSizing: "border-box" }}
+            />
+          </div>
+
+          <div
+            className={`w-[290px] h-[56px] ${
+              focusedInput === "password"
+                ? "border-none"
+                : "border-2 border-black"
+            } bg-white`}
+          >
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+              onFocus={() => setFocusedInput("password")}
+              onBlur={() => setFocusedInput(null)}
+              onChange={handleChange}
+              value={formData.password}
+              className={`placeholder-custom w-full h-full font-inter font-[300] text-[18px] leading-[21.78] p-[calc(16px-2px)] pr-[calc(23px-2px)] pl-[calc(23px-2px)] ${
+                focusedInput === "password"
+                  ? "border-2 border-black"
+                  : "border-none"
+              } bg-white`}
+              style={{ boxSizing: "border-box" }}
+            />
+          </div>
+
+          <CTA btn_name="Create account" />
+          <Google_singup_CTA btn_name="Continue with Google" />
+        </form>
+      </section>
+
+      <Toggle_login_signup
+        notify="Already have an account?"
+        redirect_To="Log in"
+        redirect_path="/login"
+      />
+    </div>
+  );
+};
+
+export default Form;
