1547eb4637fea45bfef1f09bc935d4af327dc9d8
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-06 15:57:12 +0530

    Password UI done

--- src/components/Common_Components/Top_Header.js
+++ src/components/Common_Components/Top_Header.js
@@ -1,23 +1,22 @@
 import React from "react";
 import CloseIcon from '@mui/icons-material/Close';
 
-
-const Top_Header = ({title}) => {
+const Top_Header = ({ title }) => {
   return (
     <div
-      className="h-[106px] bg-[#125B57] relative"
+      className="h-[106px] bg-[#125B57] flex justify-center items-center relative"
       style={{
         borderTopLeftRadius: '0px',
         borderTopRightRadius: '0px',
         borderBottomLeftRadius: '36px',
         borderBottomRightRadius: '36px',
-        boxShadow: '0px 4px 4px 0px #00000040' 
+        boxShadow: '0px 4px 4px 0px #00000040'
       }}
     >
-      <div className="absolute top-[40.34px] left-[18.95px]">{/* icon */}
+      <div className="absolute left-[18.95px] ">
         <CloseIcon className="text-[#FFFFFF] cursor-pointer" />
       </div>
-      <div className="absolute top-[40.34px] left-[134px]">{/* title */}
+      <div className="">
         <p className="font-inter font-[500] text-[16px] leading-[19.36px] text-[#FFFFFF]">{title}</p>
       </div>
     </div>
