500ea110253bc5dca4a16765f3a7c4a5c1c1af3c
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-07 13:24:02 +0530

    Dashboard ui done

---
+++ src/pages/HomePageDashboard/Dashboard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Top_Header from '../../components/Common_Components/Top_Header'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PersonIcon from '@mui/icons-material/Person';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import KeyIcon from '@mui/icons-material/Key';
+import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
+import HomeRepairServiceRoundedIcon from '@mui/icons-material/HomeRepairServiceRounded';
+import PowerSettingsNewRoundedIcon from '@mui/icons-material/PowerSettingsNewRounded';
+import MenuList from './MenuList';
+import LastList from './LastList';
+
+const Dashboard = () => {
+  return (
+    <div className='w-[480px] mx-auto'>
+        <Top_Header title="Home Page"/>
+        <section className='w-[314px] h-[396px] mt-[28px] mx-auto rounded-[10px]'>
+        <MenuList title='Address' isTopBorder={true}  Icon={LocationOnIcon}/>
+        <MenuList title='Account' isTopBorder={false} Icon={PersonIcon}/>
+        <MenuList title='Notification' isTopBorder={false} Icon={NotificationsIcon}/>
+        <MenuList title='Passwords' isTopBorder={false} Icon={KeyIcon}/>
+        <MenuList title='Language' isTopBorder={false} Icon={ChatRoundedIcon}/>
+        <MenuList title='Citizen Scientist kit' isTopBorder={false} Icon={HomeRepairServiceRoundedIcon}/>
+        <LastList title='Logout'  Icon={PowerSettingsNewRoundedIcon} />
+        </section>
+        
+       
+        
+    </div>
+  )
+}
+
+export default Dashboard
