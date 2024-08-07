b6ab354d018d58c7c1b49c84c913a292d34a648a
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-08-07 10:26:41 +0530

    button added

--- src/pages/myAddress/Default_Address.js
+++ src/pages/myAddress/Default_Address.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Top_Header from "../../components/Common_Components/Top_Header";
 import CTA from "../../components/Common_Components/CTA";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Default_Address = () => {
   const [focusedInput, setFocusedInput] = useState(null);
@@ -12,7 +13,7 @@ const Default_Address = () => {
     <div className="w-[480px] mx-auto">
       <Top_Header title="My Address" />
 
-      <section className=" w-[318px] h-[569px] mx-auto mt-[28px] flex flex-col gap-[328px]">
+      <section className=" w-[318px] h-[569px] mx-auto mt-[28px] flex flex-col gap-[300px]">
         <section className="w-[318px] h-[185px] flex flex-col gap-[18px] ">
           <p className="font-[600] text-[24px] leading-[26.4px] text-[#125B57]">
             Your Address
@@ -31,22 +32,22 @@ const Default_Address = () => {
               name="address"
               onFocus={() => setFocusedInput("address")}
               onBlur={() => setFocusedInput(null)}
-              className={`w-full bg-[#FAFAFA]`}
+              className={`w-full`}
               style={{
                 boxSizing: "border-box",
                 outline: focusedInput === "address" ? "none" : "",
-                
+
                 borderRadius: "2px",
-                
+                background: "white",
                 color: "#125B57",
                 fontWeight: 400,
                 fontSize: "16px",
                 lineHeight: "17.6px",
                 letterSpacing: "-0.02em",
                 textAlign: "left",
-                whiteSpace: "pre-wrap", 
-                resize: "none", 
-                height: "99px", 
+                whiteSpace: "pre-wrap",
+                resize: "none",
+                height: "99px",
               }}
               value={address}
               onChange={(e) => setAddress(e.target.value)}
@@ -54,8 +55,16 @@ const Default_Address = () => {
           </div>
         </section>
 
-        <section className=" flex justify-center">
-          <CTA btn_name="Add a New Address"/>
+        <section className=" flex justify-center items-center">
+          <button
+            className="font-inter  h-[56px] bg-[#125B57] text-white font-[500] text-[16px] leading-[19.36px] text-center rounded-[40px] pt-[10px] pr-[30px] pb-[10px] pl-[30px]"
+           
+          >
+            Add a new Address
+            <span className="pl-[10px]"><ArrowForwardIcon className="!text-[16px]"/></span>
+            
+          </button>
+              
         </section>
       </section>
     </div>
